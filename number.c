218d1a60e8847774442bdbd36c4289f0d4d52458
hirayavuzyilmaz <140586743+hirayavuzyilmaz@users.noreply.github.com>
2023-07-27 10:59:00 +0300

    Add files via upload

---
+++ number.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main(){
+
+int num1;
+
+printf("Please type a random number \n");
+scanf("%d",&num1);
+
+    if(num1>0){
+        printf("Your number is positive %f");
+
+
+    } else if (num1<0){
+        printf("Your number is negative %f");
+
+    }
+
+
+return 0;
+
+
+
+
+
+
+
+
+
+
+}
